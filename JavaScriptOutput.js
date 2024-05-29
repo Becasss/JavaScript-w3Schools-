9968f889a1fae6689edf36d8a7afdc62d7ae2f0f
Bikash Yamphu Rai <51150918+Becasss@users.noreply.github.com>
2024-05-29 10:30:14 +0545

    First Class
    I am learning JavaScript from W3Schools.om

---
+++ JavaScriptOutput.js
@@ -0,0 +1,23 @@
+// Js Display Possibilities
+
+
+// writing into an HTML element, using innerHTML.
+// writing into the HTML output using document.write().
+// writing into an alert box, using window.alert().
+// writing into the brower console, using console.log().
+
+document.getElementById('demo').innerHTML
+    = 5 + 9;
+
+
+document.write("Bikash is honest man!")
+
+
+window.alert(10 + 100);
+
+alert('Shrestha Sirjana');
+
+
+
+console.log("there are four types of
+ displying output method in JS");
