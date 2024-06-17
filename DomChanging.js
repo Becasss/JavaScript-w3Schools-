6cdda4cbc2cd209906e7e2cb8bc47526ed0253b9
Bikash Yamphu Rai <51150918+Becasss@users.noreply.github.com>
2024-06-17 23:16:00 +0545

    Html DOM- part1
    Learned about HTML DOM - to change the content of the html elements.

---
+++ DomChanging.js
@@ -0,0 +1,20 @@
+// Changing HTML Content
+document.getElementById("p1").innerHTML = "New text";
+
+
+
+
+// Changing the value of an Attribute
+
+document.getElementById("myImage").src = "babyMonk.png";
+
+
+
+// Dynamic HTML content:
+
+document.getElementById("time").innerHTML = "Date :" + Date();
+
+
+// Document.write():
+
+document.write(Date());
